7c26a83142ce35c1bb1c02d1f8ca4097f5323a06
Mustafa Harpy <mustafaharpy10@gmail.com>
2024-03-01 23:02:53 +0200

    commits

--- index.js
+++ index.js
@@ -73,6 +73,11 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+    next();
+});
+
 app.set('view engine', 'ejs');
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
