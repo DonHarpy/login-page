12503f37a4d350d6f4aa35d06441636e29a5d01d
Mustafa Harpy <mustafaharpy10@gmail.com>
2024-03-02 14:48:07 +0200

    last one inshaallah

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // connection
 const mongoose = require('mongoose')
-const connect = mongoose.connect("mongodb+srv://mustafaharpy100:Harpy222_@loginapi.ark9wv5.mongodb.net/")
+const connect = mongoose.connect("mongodb+srv://mustafaharpy100:Harpy222_@loginapi.ark9wv5.mongodb.net/qspvxooz")
 
 connect.then(()=>{
     console.log('Database connected successfuly')
